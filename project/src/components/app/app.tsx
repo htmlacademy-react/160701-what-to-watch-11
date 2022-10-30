7f28afb3632ad4ba00b6407872801a8f32e1dc04
Dmitry <sheklein.dima@gmail.com>
2022-10-30 12:33:17 +0300

    Заменит Route на PrivateRoute

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,14 @@
 import MainPage from 'src/pages/main-page/main-page';
 import { TFilmCardInfo } from 'src/pages/main-page/main-page';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
-import { AppRoute } from 'src/const';
+import { AppRoute, AuthStatus } from 'src/const';
 import Page404 from 'src/pages/404-page/404-page';
 import SingInPage from 'src/pages/sing-in-page/sing-in-page';
 import MyListPage from 'src/pages/my-list-page/my-list-page';
 import PlayerPage from 'src/pages/player-page/player-page';
 import MoviePage from 'src/pages/movie-page/movie-page';
+import PrivateRoute from '../private-route/private-route';
+import AddReviewPage from 'src/pages/add-review-page/add-review-page';
 
 type TApp = {
   film: TFilmCardInfo;
@@ -20,7 +22,23 @@ const App = (props: TApp): JSX.Element => {
       <Routes>
         <Route path={AppRoute.Root} element={<MainPage filmCardInfo={film} />} />
         <Route path={AppRoute.Login} element={<SingInPage />} />
-        <Route path={AppRoute.MyList} element={<MyListPage />} />
+        <Route
+          path={AppRoute.MyList}
+          element={
+            <PrivateRoute authStatus={AuthStatus.NoAuth}>
+              <MyListPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path={AppRoute.AddReview}
+          element={
+            <PrivateRoute authStatus={AuthStatus.NoAuth}>
+              <AddReviewPage />
+            </PrivateRoute>
+          }
+        />
+
         <Route path={AppRoute.Player} element={<PlayerPage />} />
         <Route path={AppRoute.Film} element={<MoviePage />} />
         <Route path="*" element={<Page404 />} />
