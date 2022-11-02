2903dffc393233c077cddb6d626987956525fe12
Dmitry <sheklein.dima@gmail.com>
2022-11-03 02:11:14 +0300

    Add: отрисовка через <UserLayout />

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,7 +1,7 @@
 import MainPage from 'src/pages/main-page/main-page';
 import { TFilm, TFilmCardInfo } from 'src/types/films';
 import { HelmetProvider } from 'react-helmet-async';
-import { Route, Routes, BrowserRouter, Link } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { AppRoute, AuthStatus } from 'src/const';
 import Page404 from 'src/pages/404-page/404-page';
 import SingInPage from 'src/pages/sing-in-page/sing-in-page';
@@ -10,6 +10,7 @@ import PlayerPage from 'src/pages/player-page/player-page';
 import MoviePage from 'src/pages/movie-page/movie-page';
 import PrivateRoute from '../private-route/private-route';
 import AddReviewPage from 'src/pages/add-review-page/add-review-page';
+import UserLayout from 'src/layouts/user-layout/user-layout';
 
 type TApp = {
   film: TFilmCardInfo;
@@ -24,15 +25,6 @@ const App = (props: TApp): JSX.Element => {
       <HelmetProvider>
         <Routes>
           <Route path={AppRoute.Root} element={<MainPage filmCardInfo={film} films={films} />} />
-          <Route path={AppRoute.Login} element={<SingInPage />} />
-          <Route
-            path={AppRoute.MyList}
-            element={
-              <PrivateRoute authStatus={AuthStatus.Auth}>
-                <MyListPage films={films} />
-              </PrivateRoute>
-            }
-          />
           <Route
             path={AppRoute.AddReview}
             element={
@@ -44,7 +36,18 @@ const App = (props: TApp): JSX.Element => {
 
           <Route path={AppRoute.Player} element={<PlayerPage film={films[0]} />} />
           <Route path={AppRoute.Film} element={<MoviePage films={films} />} />
-          <Route path="*" element={<Page404 />} />
+          <Route element={<UserLayout filmsCount={films.length} />}>
+            <Route
+              path={AppRoute.MyList}
+              element={
+                <PrivateRoute authStatus={AuthStatus.Auth}>
+                  <MyListPage films={films} />
+                </PrivateRoute>
+              }
+            />
+            <Route path={AppRoute.Login} element={<SingInPage />} />
+            <Route path="*" element={<Page404 />} />
+          </Route>
         </Routes>
       </HelmetProvider>
     </BrowserRouter>
--- project/src/pages/my-list-page/my-list-page.tsx
+++ project/src/pages/my-list-page/my-list-page.tsx
@@ -1,6 +1,5 @@
 import { Helmet } from 'react-helmet-async';
 import FilmsList from 'src/components/films-list/films-list';
-import Logo from 'src/components/logo/logo';
 import { PageTitles } from 'src/const';
 import { TFilm } from 'src/types/films';
 
@@ -13,41 +12,11 @@ const MyListPage = ({ films }: TMylistPage) => (
       <title>{PageTitles.MyList}</title>
     </Helmet>
 
-    <div className="user-page">
-      <header className="page-header user-page__head">
-        <Logo />
+    <section className="catalog">
+      <h2 className="catalog__title visually-hidden">Catalog</h2>
 
-        <h1 className="page-title user-page__title">
-          My list <span className="user-page__film-count">9</span>
-        </h1>
-        <ul className="user-block">
-          <li className="user-block__item">
-            <div className="user-block__avatar">
-              <img src="img/avatar.jpg" alt="User avatar" width="63" height="63" />
-            </div>
-          </li>
-          <li className="user-block__item">
-            <a href="!#" className="user-block__link">
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </header>
-
-      <section className="catalog">
-        <h2 className="catalog__title visually-hidden">Catalog</h2>
-
-        <FilmsList films={films} />
-      </section>
-
-      <footer className="page-footer">
-        <Logo isLight />
-
-        <div className="copyright">
-          <p>© 2019 What to watch Ltd.</p>
-        </div>
-      </footer>
-    </div>
+      <FilmsList films={films} />
+    </section>
   </>
 );
 
