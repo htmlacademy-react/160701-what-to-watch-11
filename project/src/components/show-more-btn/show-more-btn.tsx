eab4547f7d057af1750f893e96c083dc230e445b
Dmitry <sheklein.dima@gmail.com>
2022-11-03 04:02:42 +0300

    Add: компонент <ShowMoreBtn />

---
+++ project/src/components/show-more-btn/show-more-btn.tsx
@@ -0,0 +1,9 @@
+const ShowMoreBtn = () => (
+  <div className="catalog__more">
+    <button className="catalog__button" type="button">
+      Show more
+    </button>
+  </div>
+);
+
+export default ShowMoreBtn;
