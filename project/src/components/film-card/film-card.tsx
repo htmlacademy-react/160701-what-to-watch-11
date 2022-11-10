356ce317fc799d0b0c9302d30f1691696128196d
Dmitry <sheklein.dima@gmail.com>
2022-11-10 13:49:03 +0300

    Fix: Eslint error fix

--- project/src/components/film-card/film-card.tsx
+++ project/src/components/film-card/film-card.tsx
@@ -35,13 +35,11 @@ const FilmCard = ({ film }: TFilmCard) => {
   const isRootPage = location.pathname === AppRoute.Root;
   const isFull = isMoviePage || isAddReviewPage;
 
-  type TPropsWithChildren<P = unknown> = P & { children: JSX.Element };
+  const FilmCardHeroWrap = ({ children }: PropsWithChildren) =>
+    isMoviePage ? <div className="film-card__hero">{children}</div> : <>{children}</>;
 
-  const FilmCardHeroWrap = ({ children }: TPropsWithChildren) =>
-    isMoviePage ? <div className="film-card__hero">{children}</div> : children;
-
-  const FilmCardHeaderWrap = ({ children }: TPropsWithChildren) =>
-    isAddReviewPage ? <div className="film-card__header">{children}</div> : children;
+  const FilmCardHeaderWrap = ({ children }: PropsWithChildren) =>
+    isAddReviewPage ? <div className="film-card__header">{children}</div> : <>{children}</>;
 
   const FilmCardWrap = ({
     children,
@@ -59,68 +57,64 @@ const FilmCard = ({ film }: TFilmCard) => {
   return (
     <section className={`film-card ${isFull ? 'film-card--full' : ''}`} style={{ backgroundColor }}>
       <FilmCardHeaderWrap>
-        <>
-          <FilmCardHeroWrap>
-            <>
-              <div className="film-card__bg">
-                <img src={backgroundImage} alt={name} />
-              </div>
+        <FilmCardHeroWrap>
+          <div className="film-card__bg">
+            <img src={backgroundImage} alt={name} />
+          </div>
 
-              <h1 className="visually-hidden">WTW</h1>
+          <h1 className="visually-hidden">WTW</h1>
 
-              <Header>{isAddReviewPage && <Breadcrumbs />}</Header>
-              {isAddReviewPage && <FilmCardPoster posterImage={posterImage} name={name} isSmall />}
-              {isMoviePage && (
-                <FilmCardWrap>
-                  <FilmCardDescr>
-                    <FilmCardData name={name} genre={genre} released={released} />
-                    <FilmCardButtons id={id} withReviewLink />
-                  </FilmCardDescr>
-                </FilmCardWrap>
-              )}
-            </>
-          </FilmCardHeroWrap>
-
+          <Header>{isAddReviewPage && <Breadcrumbs />}</Header>
+          {isAddReviewPage && <FilmCardPoster posterImage={posterImage} name={name} isSmall />}
           {isMoviePage && (
-            <FilmCardWrap className="film-card__translate-top">
-              <FilmCardInfo>
-                <FilmCardPoster posterImage={posterImage} name={name} isBig />
-                <FilmCardDescr>
-                  <FilmNav />
-                  <div className="film-rating">
-                    <div className="film-rating__score">{rating}</div>
-                    <p className="film-rating__meta">
-                      <span className="film-rating__level">{getFilmRatingPhrase(rating)}</span>
-                      <span className="film-rating__count">{scoresCount} ratings</span>
-                    </p>
-                  </div>
-                  <div className="film-card__text">
-                    <p>{description}</p>
-
-                    <p className="film-card__director">
-                      <strong>Director: {director}</strong>
-                    </p>
-
-                    <p className="film-card__starring">
-                      <strong>Starring: {starring.join(',')} and other</strong>
-                    </p>
-                  </div>
-                </FilmCardDescr>
-              </FilmCardInfo>
-            </FilmCardWrap>
-          )}
-          {isRootPage && (
             <FilmCardWrap>
-              <FilmCardInfo>
-                <FilmCardPoster posterImage={posterImage} name={name} />
-                <FilmCardDescr>
-                  <FilmCardData name={name} genre={genre} released={released} />
-                  <FilmCardButtons />
-                </FilmCardDescr>
-              </FilmCardInfo>
+              <FilmCardDescr>
+                <FilmCardData name={name} genre={genre} released={released} />
+                <FilmCardButtons id={id} withReviewLink />
+              </FilmCardDescr>
             </FilmCardWrap>
           )}
-        </>
+        </FilmCardHeroWrap>
+
+        {isMoviePage && (
+          <FilmCardWrap className="film-card__translate-top">
+            <FilmCardInfo>
+              <FilmCardPoster posterImage={posterImage} name={name} isBig />
+              <FilmCardDescr>
+                <FilmNav />
+                <div className="film-rating">
+                  <div className="film-rating__score">{rating}</div>
+                  <p className="film-rating__meta">
+                    <span className="film-rating__level">{getFilmRatingPhrase(rating)}</span>
+                    <span className="film-rating__count">{scoresCount} ratings</span>
+                  </p>
+                </div>
+                <div className="film-card__text">
+                  <p>{description}</p>
+
+                  <p className="film-card__director">
+                    <strong>Director: {director}</strong>
+                  </p>
+
+                  <p className="film-card__starring">
+                    <strong>Starring: {starring.join(',')} and other</strong>
+                  </p>
+                </div>
+              </FilmCardDescr>
+            </FilmCardInfo>
+          </FilmCardWrap>
+        )}
+        {isRootPage && (
+          <FilmCardWrap>
+            <FilmCardInfo>
+              <FilmCardPoster posterImage={posterImage} name={name} />
+              <FilmCardDescr>
+                <FilmCardData name={name} genre={genre} released={released} />
+                <FilmCardButtons />
+              </FilmCardDescr>
+            </FilmCardInfo>
+          </FilmCardWrap>
+        )}
       </FilmCardHeaderWrap>
       {isAddReviewPage && <AddReviewForm backgroundColor={adjustColor(backgroundColor, 10)} />}
     </section>
