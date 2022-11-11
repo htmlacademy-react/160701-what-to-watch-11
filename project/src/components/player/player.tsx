ea8101b4610eca9227e234105d4eab7d34d64268
Dmitry <sheklein.dima@gmail.com>
2022-11-11 16:31:54 +0300

    Fix: типизация <Player />

--- project/src/components/player/player.tsx
+++ project/src/components/player/player.tsx
@@ -1,9 +1,3 @@
-import { DetailedHTMLProps, VideoHTMLAttributes } from 'react';
-
-type TPlayer = DetailedHTMLProps<VideoHTMLAttributes<HTMLVideoElement>, HTMLVideoElement>;
-
-const Player = ({ autoPlay = false, ...props }: TPlayer) => (
-  <video autoPlay={autoPlay} {...props} />
-);
+const Player = ({ ...props }) => <video {...props} />;
 
 export default Player;
