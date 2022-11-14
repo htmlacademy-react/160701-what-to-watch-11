de31297d7e2cf821fa453af6296350ca8fd5e7be
Dmitry <sheklein.dima@gmail.com>
2022-11-14 15:40:03 +0300

    Fix: отрисовка карточки фильма через тег video

--- project/src/components/small-film-card/small-film-card.tsx
+++ project/src/components/small-film-card/small-film-card.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import { TFilmCard } from 'src/types/films';
 import Player from '../player/player';
 
-const SmallFilmCard = ({ film, withVideo, onMouseOver, onMouseLeave }: TFilmCard) => {
+const SmallFilmCard = ({ film, playing, onMouseOver, onMouseLeave }: TFilmCard) => {
   const { id, name, posterImage, previewVideoLink } = film;
 
   const ImageSize = {
@@ -17,19 +17,15 @@ const SmallFilmCard = ({ film, withVideo, onMouseOver, onMouseLeave }: TFilmCard
       onMouseLeave={onMouseLeave}
     >
       <div className="small-film-card__image">
-        {withVideo ? (
-          <Player
-            poster={posterImage}
-            src={previewVideoLink}
-            width={ImageSize.Width}
-            height={ImageSize.Height}
-            autoPlay
-            loop
-            muted
-          />
-        ) : (
-          <img src={posterImage} alt={name} width={ImageSize.Width} height={ImageSize.Height} />
-        )}
+        <Player
+          poster={posterImage}
+          src={previewVideoLink}
+          width={ImageSize.Width}
+          height={ImageSize.Height}
+          playing={playing}
+          loop
+          muted
+        />
       </div>
       <h3 className="small-film-card__title">
         <Link className="small-film-card__link" to={`/films/${id}`}>
--- project/src/types/films.ts
+++ project/src/types/films.ts
@@ -25,7 +25,7 @@ export type TFilmCardInfo = Pick<
 
 export type TFilmCard = {
   film: Pick<TFilm, 'id' | 'name' | 'posterImage' | 'previewImage' | 'previewVideoLink'>;
-  withVideo?: boolean;
+  playing: boolean;
   onMouseOver: (id: number) => void;
   onMouseLeave: () => void;
 };
