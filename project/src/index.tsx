f5a9a3481bdbedda5b29a12a164163a88e985c84
Dmitry <sheklein.dima@gmail.com>
2022-11-14 11:28:24 +0300

    Подключит redux к проекту.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import films from './mocks/films';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App films={films} />
+    <Provider store={store}>
+      <App films={films} />
+    </Provider>
   </React.StrictMode>,
 );
