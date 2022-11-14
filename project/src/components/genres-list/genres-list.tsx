08fa5754d8c2565e9caf04f1e48b03361efa0222
Dmitry <sheklein.dima@gmail.com>
2022-11-14 12:08:11 +0300

    Add: доработка <GenresList /> для вывода жанров

--- project/src/components/genres-list/genres-list.tsx
+++ project/src/components/genres-list/genres-list.tsx
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
+import { TFilm } from 'src/types/films';
 
-const GenresList = () => {
-  const sampleGenres = [' All genres', 'Comedies', 'Crime', 'Documentary', 'Dramas', 'Horror'];
+type TGenresList = {
+  films: TFilm[];
+};
+
+const GenresList = ({ films }: TGenresList) => {
+  const DEFAULT_NAME_GENRE = 'All genres';
+  const MAX_FILMS_GENRES = 10;
+  const filmsGenres = films.map((film) => film.genre);
+  const genres = Array.from(new Set([DEFAULT_NAME_GENRE, ...filmsGenres])).slice(
+    0,
+    MAX_FILMS_GENRES,
+  );
 
   return (
     <ul className="catalog__genres-list">
-      {sampleGenres.map((item, idx) => (
+      {genres.map((item, idx) => (
         <li
           className={`catalog__genres-item ${idx === 0 ? 'catalog__genres-item--active' : ''}`}
           key={item}
