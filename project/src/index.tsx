024f47b38a111b2148d17d0d6db043546892833b
Dmitry <sheklein.dima@gmail.com>
2022-11-15 20:36:54 +0300

    Fix: изменяет проброс данных при старте

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import mockFilms from 'src/mocks/films';
+import { setAllFilms, setCurrentFilm } from 'src/store/action';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
+store.dispatch(setAllFilms(mockFilms));
+store.dispatch(setCurrentFilm(mockFilms[0]));
 
 root.render(
   <React.StrictMode>
