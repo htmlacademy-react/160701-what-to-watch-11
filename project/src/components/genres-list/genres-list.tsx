85aafcc7f5f03b6f2a655fcbc41bdcf804be9e91
Dmitry <sheklein.dima@gmail.com>
2022-11-15 21:12:23 +0300

    Feat: доработка <GenresList />

--- project/src/components/genres-list/genres-list.tsx
+++ project/src/components/genres-list/genres-list.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
 import { AppRoute, RouteName } from 'src/const';
 import { TFilm } from 'src/types/films';
 
@@ -7,6 +8,8 @@ type TGenresList = {
 };
 
 const GenresList = ({ films }: TGenresList) => {
+  const location = useLocation();
+  const hash = decodeURI(location.hash.slice(1));
   const DEFAULT_NAME_GENRE = 'All genres';
   const MAX_FILMS_GENRES = 10;
   const filmsGenres = films.map((film) => film.genre);
@@ -14,17 +17,28 @@ const GenresList = ({ films }: TGenresList) => {
     0,
     MAX_FILMS_GENRES,
   );
+  const [active, setActive] = useState(hash || DEFAULT_NAME_GENRE);
 
   return (
     <ul className="catalog__genres-list">
-      {genres.map((item, idx) => (
+      {genres.map((item) => (
         <li
-          className={`catalog__genres-item ${idx === 0 ? 'catalog__genres-item--active' : ''}`}
+          className={`catalog__genres-item ${
+            item === active ? 'catalog__genres-item--active' : ''
+          }`}
           key={item}
         >
-          <Link className="catalog__genres-link" to={AppRoute.Root}>
+          <a
+            className="catalog__genres-link"
+            href="!#"
+            onClick={(evt) => {
+              evt.preventDefault();
+              window.location.hash = `#${item}`;
+              setActive(item);
+            }}
+          >
             {item}
-          </Link>
+          </a>
         </li>
       ))}
     </ul>
