c9ff7b8e0073254ffce42263162b833ec414a765
Dmitry <sheklein.dima@gmail.com>
2022-11-15 09:56:22 +0300

    Доработка отображения фильма в <PlayerPage />

--- project/src/pages/player-page/player-page.tsx
+++ project/src/pages/player-page/player-page.tsx
@@ -1,57 +1,65 @@
 import { Helmet } from 'react-helmet-async';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useParams } from 'react-router-dom';
 import { AppRoute, PageTitles } from 'src/const';
 import { TFilm } from 'src/types/films';
 
 type TPlayerPage = {
-  film: TFilm;
+  films: TFilm[];
 };
-const PlayerPage = ({ film: { previewImage, videoLink } }: TPlayerPage) => (
-  <>
-    <Helmet>
-      <title>{PageTitles.Player}</title>
-    </Helmet>
 
-    <div className="player">
-      <video src={videoLink} className="player__video" poster={previewImage}></video>
+const PlayerPage = ({ films }: TPlayerPage) => {
+  const { id: currentFilmId } = useParams();
+  const currentFilm = films.find((film) => film.id === Number(currentFilmId));
 
-      {/* <button type="button" className="player__exit">
-        Exit
-      </button> */}
-      <Link className="player__exit" to={AppRoute.Root}>
-        Exit
-      </Link>
+  if (!currentFilm) {
+    return <Navigate to={AppRoute.Root} />;
+  }
+  const { previewImage, videoLink } = currentFilm;
 
-      <div className="player__controls">
-        <div className="player__controls-row">
-          <div className="player__time">
-            <progress className="player__progress" value="30" max="100"></progress>
-            <div className="player__toggler" style={{ left: '30%' }}>
-              Toggler
+  return (
+    <>
+      <Helmet>
+        <title>{PageTitles.Player}</title>
+      </Helmet>
+
+      <div className="player">
+        <video src={videoLink} className="player__video" poster={previewImage}></video>
+
+        <Link className="player__exit" to={`/films/${currentFilm.id}`}>
+          Exit
+        </Link>
+
+        <div className="player__controls">
+          <div className="player__controls-row">
+            <div className="player__time">
+              <progress className="player__progress" value="30" max="100"></progress>
+              <div className="player__toggler" style={{ left: '30%' }}>
+                Toggler
+              </div>
             </div>
+            <div className="player__time-value">1:30:29</div>
           </div>
-          <div className="player__time-value">1:30:29</div>
-        </div>
 
-        <div className="player__controls-row">
-          <button type="button" className="player__play">
-            <svg viewBox="0 0 19 19" width="19" height="19">
-              <use xlinkHref="#play-s"></use>
-            </svg>
-            <span>Play</span>
-          </button>
-          <div className="player__name">Transpotting</div>
+          <div className="player__controls-row">
+            <button type="button" className="player__play">
+              <svg viewBox="0 0 19 19" width="19" height="19">
+                <use xlinkHref="#play-s"></use>
+              </svg>
+              <span>Play</span>
+            </button>
+            <div className="player__name">Transpotting</div>
 
-          <button type="button" className="player__full-screen">
-            <svg viewBox="0 0 27 27" width="27" height="27">
-              <use xlinkHref="#full-screen"></use>
-            </svg>
-            <span>Full screen</span>
-          </button>
+            <button type="button" className="player__full-screen">
+              <svg viewBox="0 0 27 27" width="27" height="27">
+                <use xlinkHref="#full-screen"></use>
+              </svg>
+              <span>Full screen</span>
+            </button>
+          </div>
         </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default PlayerPage;
