0f9b78bf3703316032a13f37579a7408931b47bd
Дмитрий <shcheklein.dev@gmail.com>
2022-11-20 17:03:19 +0300

    Fix: отрисовка <FilmCard />

--- project/src/layouts/film-card-layout/film-card-layout.tsx
+++ project/src/layouts/film-card-layout/film-card-layout.tsx
@@ -1,26 +1,19 @@
-import { Navigate, Outlet, useLocation, useParams } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import FilmCard from 'src/components/film-card/film-card';
 import Footer from 'src/components/footer/footer';
-import { AppRoute } from 'src/const';
+import { RouteName } from 'src/const';
 import { TFilm } from 'src/types/films';
 
 type TFilmCardLayout = {
   films: TFilm[];
 };
 const FilmCardLayout = ({ films }: TFilmCardLayout) => {
-  const DEFAULT_FILM_ID = films[0].id;
-  const { id: currentFilmId = DEFAULT_FILM_ID } = useParams();
-  const currentFilm = films.find((film) => film.id === Number(currentFilmId));
   const location = useLocation();
-  const isAddReviewPage = location.pathname.includes('review');
-
-  if (!currentFilm) {
-    return <Navigate to={AppRoute.Root} />;
-  }
+  const isAddReviewPage = location.pathname.includes(RouteName.Review);
 
   return (
     <>
-      <FilmCard film={currentFilm} />
+      <FilmCard films={films} />
       {isAddReviewPage ? (
         <Outlet />
       ) : (
