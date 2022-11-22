de84763293fad3dd8ed99c3ffa3735efdeb417a4
Dmitry <sheklein.dima@gmail.com>
2022-11-22 13:38:09 +0300

    Fix: сортировка фильмов

--- project/src/pages/main-page/main-page.tsx
+++ project/src/pages/main-page/main-page.tsx
@@ -1,8 +1,11 @@
+import { useEffect } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useLocation } from 'react-router-dom';
 import FilmsList from 'src/components/films-list/films-list';
 import GenresList from 'src/components/genres-list/genres-list';
 import { DEFAULT_NAME_GENRE, PageTitles } from 'src/const';
+import { useAppDispatch, useAppSelector } from 'src/hooks';
+import { changeCurrentGenre } from 'src/store/action';
 import { TFilm } from 'src/types/films';
 
 type TMainPage = {
@@ -10,12 +13,24 @@ type TMainPage = {
 };
 
 const MainPage = ({ films }: TMainPage) => {
+  const dispatch = useAppDispatch();
   const location = useLocation();
   const hash = decodeURI(location.hash.slice(1));
-  const filteredBy = hash || DEFAULT_NAME_GENRE;
+  const stateGenre = useAppSelector((state) => state.currentGenre);
   const sortedFilms = films.filter(
-    (film) => filteredBy === DEFAULT_NAME_GENRE || filteredBy === film.genre,
+    (film) => stateGenre === DEFAULT_NAME_GENRE || stateGenre === film.genre,
   );
+
+  useEffect(() => {
+    const isCorrectHash =
+      hash && hash !== DEFAULT_NAME_GENRE && films.some((film) => film.genre === hash);
+    if (isCorrectHash) {
+      dispatch(changeCurrentGenre(hash));
+    }
+    return () => {
+      dispatch(changeCurrentGenre(DEFAULT_NAME_GENRE));
+    };
+  }, [dispatch, hash, films]);
 
   return (
     <>
