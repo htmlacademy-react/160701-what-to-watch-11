5334843c42f0d15cfa26b133f4dfb98dd87f3984
Дмитрий <shcheklein.dev@gmail.com>
2022-12-04 12:13:29 +0300

    Fix: переработка action

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -3,50 +3,32 @@ import { AuthStatus } from 'src/const';
 import { TFilm, TFilmComment } from 'src/types/films';
 import { UserData } from 'src/types/user-data';
 
-const changeCurrentGenre = createAction('films/currentGenre', (value: string) => ({
-  payload: value,
-}));
+const changeCurrentGenre = createAction<string>('films/currentGenre');
+const changeCurrentFilm = createAction<TFilm>('films/currentFilm');
 
-const changeCurrentFilm = createAction('films/currentFilm', (value: TFilm) => ({
-  payload: value,
-}));
+const setAllFilms = createAction<TFilm[]>('films/setAllFilms');
+const setFilmsLoadingStatus = createAction<boolean>('films/setFilmsLoadingStatus');
 
-const setAllFilms = createAction('films/getAll', (value: TFilm[]) => ({
-  payload: value,
-}));
+const setSimilarFilms = createAction<TFilm[]>('films/setSimilarFilms');
+const setSimilarFilmsLoadingStatus = createAction<boolean>('films/setSimilarFilmsLoadingStatus');
 
-const loadFilms = createAction<TFilm[]>('data/loadFilms');
-
-const loadSimilarFilms = createAction('data/loadSimilarFilms', (value: TFilm[]) => ({
-  payload: value,
-}));
-const loadFilmComments = createAction('data/loadFilmComments', (value: TFilmComment[]) => ({
-  payload: value,
-}));
-
-const requireAuthorization = createAction<AuthStatus>('user/requireAuthorization');
-
-const setCommentsLoadingStatus = createAction<boolean>('data/setCommentsLoadingStatus');
-const setFilmsLoadingStatus = createAction<boolean>('data/setFilmsLoadingStatus');
-const setSimilarFilmsLoadingStatus = createAction<boolean>('data/setSimilarFilmsLoadingStatus');
+const setFilmComments = createAction<TFilmComment[]>('films/setFilmComments');
+const setCommentsLoadingStatus = createAction<boolean>('films/setCommentsLoadingStatus');
 
 const setError = createAction<string | null>('app/setEror');
-
-const setUser = createAction('user/setUser', (value: UserData) => ({
-  payload: value,
-}));
+const setUser = createAction<UserData>('user/setUser');
+const setAuthorizationStatus = createAction<AuthStatus>('user/setAuthorizationStatus');
 
 export {
   changeCurrentFilm,
   changeCurrentGenre,
   setAllFilms,
-  loadFilms,
-  loadSimilarFilms,
-  requireAuthorization,
+  setSimilarFilms,
+  setAuthorizationStatus,
   setFilmsLoadingStatus,
   setSimilarFilmsLoadingStatus,
   setCommentsLoadingStatus,
-  loadFilmComments,
+  setFilmComments,
   setError,
   setUser,
 };
