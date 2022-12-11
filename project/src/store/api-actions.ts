26412c4359d77d5382d1981898beb6415b92c804
Дмитрий <shcheklein.dev@gmail.com>
2022-12-11 23:42:05 +0300

    Refactor: api-actions

--- project/src/store/api-actions.ts
+++ project/src/store/api-actions.ts
@@ -1,19 +1,8 @@
 import { AxiosInstance } from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import {
-  // changeCurrentFilm,
-  // setError,
-  // setFilmsLoadingStatus,
-  // setSimilarFilmsLoadingStatus,
-  // setCommentsLoadingStatus,
-  // setAllFilms,
-  // setSimilarFilms,
-  // setFilmComments,
-  redirectToRoute,
-  // setCurrentFilmLoadingEnd,
-} from './action';
+import { redirectToRoute } from './action';
 import { TAppDispatch, TState } from 'src/types/state';
-import { APIRoute, APIRouteName, RouteName, TIMEOUT_SHOW_ERROR } from 'src/const';
+import { APIRoute, APIRouteName, RouteName } from 'src/const';
 import { TFilm, TFilmComment } from 'src/types/films';
 import { AuthData } from 'src/types/auth-data';
 import { UserData } from 'src/types/user-data';
@@ -32,6 +21,7 @@ const fetchFilmAction = createAsyncThunk<TFilm, number | string, ThunkApiConfig>
   'data/fetchFilm',
   async (filmId, { extra: api }) => {
     const { data } = await api.get<TFilm>(`${APIRoute.Films}/${filmId}`);
+
     return data;
   },
 );
@@ -39,22 +29,18 @@ const fetchFilmAction = createAsyncThunk<TFilm, number | string, ThunkApiConfig>
 const fetchSimilarFilmsAction = createAsyncThunk<TFilm[], number | string, ThunkApiConfig>(
   'data/fetchSimilarFilm',
   async (filmId, { extra: api }) => {
-    // dispatch(setSimilarFilmsLoadingStatus(true));
     const { data } = await api.get<TFilm[]>(`${APIRoute.Films}/${filmId}/${APIRouteName.Similar}`);
+
     return data;
-    // dispatch(setSimilarFilmsLoadingStatus(false));
-    // dispatch(setSimilarFilms(data));
   },
 );
 
 const fetchCommentsFilmAction = createAsyncThunk<TFilmComment[], number | string, ThunkApiConfig>(
   'data/fetchFilmComments',
-  async (filmId, { dispatch, extra: api }) => {
-    // dispatch(setCommentsLoadingStatus(true));
+  async (filmId, { extra: api }) => {
     const { data } = await api.get<TFilmComment[]>(`${APIRoute.Comments}/${filmId}`);
+
     return data;
-    // dispatch(setCommentsLoadingStatus(false));
-    // dispatch(setFilmComments(data));
   },
 );
 
@@ -69,12 +55,10 @@ const addCommentFilmAction = createAsyncThunk<
 
 const fetchFilmsAction = createAsyncThunk<TFilm[], undefined, ThunkApiConfig>(
   'data/fetchFilms',
-  async (_arg, { dispatch, extra: api }) => {
-    // dispatch(setFilmsLoadingStatus(true));
+  async (_arg, { extra: api }) => {
     const { data } = await api.get<TFilm[]>(APIRoute.Films);
+
     return data;
-    // dispatch(setFilmsLoadingStatus(false));
-    // dispatch(setAllFilms(data));
   },
 );
 
