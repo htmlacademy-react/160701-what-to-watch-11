512fdca8c824800f0fa0ecfe1b7bd8db83a6f7ca
Dmitry <sheklein.dima@gmail.com>
2022-12-12 23:39:58 +0300

    Feat: <Breadcrumbs />

--- project/src/components/breadcrumbs/breadcrumbs.tsx
+++ project/src/components/breadcrumbs/breadcrumbs.tsx
@@ -1,19 +1,29 @@
-//TODO оживить хлебные крошки
-const Breadcrumbs = () => (
-  <nav className="breadcrumbs">
-    <ul className="breadcrumbs__list">
-      <li className="breadcrumbs__item">
-        <a href="film-page.html" className="breadcrumbs__link">
-          The Grand Budapest Hotel
-        </a>
-      </li>
-      <li className="breadcrumbs__item">
-        <a href="!#" className="breadcrumbs__link">
-          Add review
-        </a>
-      </li>
-    </ul>
-  </nav>
-);
+import { Link } from 'react-router-dom';
+import { RouteName } from 'src/const';
+import { useAppSelector } from 'src/hooks';
+import { getCurrentFilm } from 'src/store/films-process/selectors';
+
+const Breadcrumbs = () => {
+  const currenFilm = useAppSelector(getCurrentFilm);
+
+  if (!currenFilm) {
+    return null;
+  }
+  const { id, name } = currenFilm;
+  return (
+    <nav className="breadcrumbs">
+      <ul className="breadcrumbs__list">
+        <li className="breadcrumbs__item">
+          <Link className="breadcrumbs__link" to={`${RouteName.Films}/${id}`}>
+            {name}
+          </Link>
+        </li>
+        <li className="breadcrumbs__item">
+          <span className="breadcrumbs__link">Add review</span>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Breadcrumbs;
