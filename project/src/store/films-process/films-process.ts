8bd528ebcd2210d164d4c8f204a6b3d0a4f7b8e9
Дмитрий <shcheklein.dev@gmail.com>
2022-12-14 12:30:06 +0300

    Fix: films-process

--- project/src/store/films-process/films-process.ts
+++ project/src/store/films-process/films-process.ts
@@ -11,7 +11,7 @@ import {
   fetchSimilarFilmsAction,
 } from '../api-actions';
 
-type TInitialState = {
+export type TInitialState = {
   films: {
     currentGenre: string;
     currentFilm: TFilm | null;
@@ -27,7 +27,7 @@ type TInitialState = {
     loading: boolean;
   };
 };
-const initialState: TInitialState = {
+export const initialState: TInitialState = {
   films: {
     currentGenre: DEFAULT_NAME_GENRE,
     currentFilm: null,
@@ -65,13 +65,13 @@ export const filmsProcess = createSlice({
 
     builder.addCase(fetchFilmAction.fulfilled, (state, action) => {
       state.films.currentFilm = action.payload;
-      state.films.currentFilmLoading = true;
-    });
-    builder.addCase(fetchFilmAction.pending, (state) => {
       state.films.currentFilmLoading = false;
     });
-    builder.addCase(fetchFilmAction.rejected, (state) => {
+    builder.addCase(fetchFilmAction.pending, (state) => {
       state.films.currentFilmLoading = true;
+    });
+    builder.addCase(fetchFilmAction.rejected, (state) => {
+      state.films.currentFilmLoading = false;
     });
 
     builder.addCase(fetchFavoriteFilmsAction.fulfilled, (state, action) => {
@@ -90,10 +90,10 @@ export const filmsProcess = createSlice({
       state.films.similarLoading = false;
     });
     builder.addCase(fetchSimilarFilmsAction.pending, (state) => {
-      state.films.similarLoading = false;
+      state.films.similarLoading = true;
     });
     builder.addCase(fetchSimilarFilmsAction.rejected, (state) => {
-      state.films.similarLoading = true;
+      state.films.similarLoading = false;
     });
 
     builder.addCase(fetchCommentsFilmAction.fulfilled, (state, action) => {
