49b57407d0801f51d23cf7c85c6a78e37c7adcf6
Дмитрий <shcheklein.dev@gmail.com>
2022-12-15 11:29:33 +0300

    Fix: получение списка избранного

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import { Provider } from 'react-redux';
 import { store } from './store';
-import { checkAuthAction, fetchFavoriteFilmsAction, fetchFilmsAction } from 'src/store/api-actions';
+import { checkAuthAction, fetchFilmsAction } from 'src/store/api-actions';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 store.dispatch(checkAuthAction());
 store.dispatch(fetchFilmsAction());
-store.dispatch(fetchFavoriteFilmsAction());
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
