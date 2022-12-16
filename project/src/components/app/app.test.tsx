4c484b1a8a744f6c84e7a53293efcf7b0d6d04c8
Dmitry <sheklein.dima@gmail.com>
2022-12-16 21:55:42 +0300

    Fix: app.test.tsx

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -1,6 +1,6 @@
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { createMemoryHistory } from 'history';
-import { AppRoute, AuthStatus, DEFAULT_NAME_GENRE, NameSpace, RouteName } from 'src/const';
+import { AppRoute, AuthStatus, NameSpace, RouteName } from 'src/const';
 import { Provider } from 'react-redux';
 import HistoryRouter from '../history-route/history-route';
 import App from './app';
@@ -8,36 +8,53 @@ import { render, screen } from '@testing-library/react';
 import { makeFakeFilmsArray } from 'src/utils/mocks';
 import thunk from 'redux-thunk';
 import { TabsNames } from '../film-card/film-card';
-import { changeAuthStatus } from 'src/store/user-process/user-process';
+import {
+  initialState as userState,
+  TInitialState as TUserState,
+} from 'src/store/user-process/user-process';
+import {
+  initialState as filmsState,
+  TInitialState as TFilmsState,
+} from 'src/store/films-process/films-process';
 
 const mockStore = configureMockStore([thunk]);
 const fakeAllFilms = makeFakeFilmsArray();
+const [fakeFilm] = fakeAllFilms;
 
-const store = mockStore({
-  [NameSpace.User]: { authorizationStatus: AuthStatus.NoAuth, userData: null },
+const storeNoAuth = mockStore({
+  [NameSpace.User]: {
+    ...userState,
+    authorizationStatus: AuthStatus.NoAuth,
+  } as TUserState,
   [NameSpace.Films]: {
+    ...filmsState,
     films: {
-      currentGenre: DEFAULT_NAME_GENRE,
-      currentFilm: fakeAllFilms[0],
-      currentFilmLoading: false,
+      ...filmsState.films,
       all: fakeAllFilms,
-      similar: makeFakeFilmsArray(),
-      allLoading: false,
-      similarLoading: false,
-      favorite: [],
+      currentFilm: fakeFilm,
     },
-    comments: {
-      data: [],
-      loading: false,
-      addCommentLoading: false,
+  } as TFilmsState,
+});
+
+const storeAuth = mockStore({
+  [NameSpace.User]: {
+    ...userState,
+    authorizationStatus: AuthStatus.Auth,
+  } as TUserState,
+  [NameSpace.Films]: {
+    ...filmsState,
+    films: {
+      ...filmsState.films,
+      all: fakeAllFilms,
+      currentFilm: fakeFilm,
     },
-  },
+  } as TFilmsState,
 });
 
 const history = createMemoryHistory();
 
-const fakeApp = (
-  <Provider store={store}>
+const fakeApp = (storeType = AuthStatus.NoAuth) => (
+  <Provider store={storeType === AuthStatus.NoAuth ? storeNoAuth : storeAuth}>
     <HistoryRouter history={history}>
       <App />
     </HistoryRouter>
@@ -53,56 +70,59 @@ describe('Application Routing', () => {
   });
   it(`should render "index page" when user navigate to "${AppRoute.Root}"`, () => {
     history.push(AppRoute.Root);
-    render(fakeApp);
+    render(fakeApp());
     expect(screen.getByText(/All genres/i)).toBeInTheDocument();
   });
   it(`should render "AuthScreen" when user navigate to "${AppRoute.Login}"`, () => {
     history.push(AppRoute.Login);
 
-    render(fakeApp);
+    render(fakeApp());
 
     // expect(screen.getByRole('heading')).toHaveTextContent('Sing in');
     // expect(screen.getByText(/Sing in/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/Email address/i)).toBeInTheDocument();
     expect(screen.getByPlaceholderText(/Password/i)).toBeInTheDocument();
   });
-  it(`should render "MovieScreen" when user navigate to "/${RouteName.Films}/${fakeAllFilms[0].id}}"`, () => {
-    const fakeFilm = fakeAllFilms[0];
-
+  it(`should render "MovieScreen" when user navigate to "/${RouteName.Films}/${fakeFilm.id}}"`, () => {
     history.push(`/${RouteName.Films}/${fakeFilm.id}`);
 
-    render(fakeApp);
+    render(fakeApp());
     // expect(screen.getByText(fakeFilm.name)).toBeInTheDocument();
     expect(screen.getByText(TabsNames.Overview)).toBeInTheDocument();
   });
-  it(`should render "PlayerScreen" when user navigate to "/${RouteName.Player}/${fakeAllFilms[0].id}"`, () => {
-    const fakeFilm = fakeAllFilms[0];
-
+  it(`should render "PlayerScreen" when user navigate to "/${RouteName.Player}/${fakeFilm.id}"`, () => {
     history.push(`/${RouteName.Player}/${fakeFilm.id}`);
-    render(fakeApp);
+    render(fakeApp());
     expect(screen.getByText(fakeFilm.name)).toBeInTheDocument();
     expect(screen.getByText(/Exit/i)).toBeInTheDocument();
   });
   it(`should render "Error404Screen" when user navigate to "${AppRoute.ErrorPage}`, () => {
     history.push(AppRoute.ErrorPage);
-    render(fakeApp);
+    render(fakeApp());
 
     expect(screen.getByText('404 Not Found')).toBeInTheDocument();
     expect(screen.getByText('Вернуться на главную')).toBeInTheDocument();
   });
   it(`should render "MyListScreen" when user navigate to "/${AppRoute.MyList}`, () => {
-    store.dispatch(changeAuthStatus(AuthStatus.Auth));
-
     history.push(AppRoute.MyList);
 
-    render(fakeApp);
+    render(fakeApp(AuthStatus.Auth));
     expect(screen.getByText('Catalog')).toBeInTheDocument();
+    expect(screen.getByText('My list')).toBeInTheDocument();
+  });
+  it(`should render "AddReviewScreen" when user navigate to "/${RouteName.Films}/${fakeFilm.id}/${RouteName.Review}`, () => {
+    history.push(`/${RouteName.Films}/${fakeFilm.id}/${RouteName.Review}`);
+
+    render(fakeApp(AuthStatus.Auth));
+    expect(screen.getByText(fakeFilm.name)).toBeInTheDocument();
+    expect(screen.getByText('Add review')).toBeInTheDocument();
+    expect(screen.getByPlaceholderText(/Review text/i)).toBeInTheDocument();
   });
 
   it('should render "NotFoundScreen" when user navigate to non-existent route', () => {
     history.push('/non-existent-route');
 
-    render(fakeApp);
+    render(fakeApp());
 
     expect(screen.getByText('404 Not Found')).toBeInTheDocument();
     expect(screen.getByText('Вернуться на главную')).toBeInTheDocument();
