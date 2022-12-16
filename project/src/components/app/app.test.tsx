ab2279675a4f28103a58e08803a7283b9b0e15a9
Дмитрий <shcheklein.dev@gmail.com>
2022-12-16 12:29:49 +0300

    Fix: app.test.tsx

--- project/src/components/app/app.test.tsx
+++ project/src/components/app/app.test.tsx
@@ -6,10 +6,11 @@ import HistoryRouter from '../history-route/history-route';
 import App from './app';
 import { render, screen } from '@testing-library/react';
 import { makeFakeFilmsArray, makeFakeFilm } from 'src/utils/mocks';
+import thunk from 'redux-thunk';
 
 const fakeFilms = makeFakeFilmsArray();
 const fakeFilm = makeFakeFilm();
-const mockStore = configureMockStore();
+const mockStore = configureMockStore([thunk]);
 
 const store = mockStore({
   [NameSpace.User]: { authorizationStatus: AuthStatus.Auth, userData: null },
@@ -49,18 +50,19 @@ describe('Application Routing', () => {
     window.HTMLMediaElement.prototype.load = jest.fn();
     window.scrollTo = jest.fn();
   });
-  // it('should render "index page" when user navigate to "/"', () => {
-  //   history.push(AppRoute.Root);
-  //   render(fakeApp);
-  //   expect(screen.getByText(/All genres/i)).toBeInTheDocument();
-  // });
-  // it('should render "AuthScreen" when user navigate to "/login"', () => {
-  //   history.push(AppRoute.Login);
+  it('should render "index page" when user navigate to "/"', () => {
+    history.push(AppRoute.Root);
+    render(fakeApp);
+    expect(screen.getByText(/All genres/i)).toBeInTheDocument();
+  });
+  it('should render "AuthScreen" when user navigate to "/login"', () => {
+    history.push(AppRoute.Login);
 
-  //   render(fakeApp);
+    render(fakeApp);
 
-  //   expect(screen.getByText(/Sign in/i)).toBeInTheDocument();
-  // });
+    // expect(screen.getByRole('heading')).toHaveTextContent('Sing in');
+    expect(screen.getByText(/Sing in/i)).toBeInTheDocument();
+  });
 
   it('should render "NotFoundScreen" when user navigate to non-existent route', () => {
     history.push('/non-existent-route');
